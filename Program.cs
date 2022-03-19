88e4806416dec7a1c2e5d947e059e75abfbfc0ed
chrisbal05 <101019395+chrisbal05@users.noreply.github.com>
2022-03-19 19:43:29 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Dice_Roll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random numberGen = new Random();
+
+            int roll = 0;
+            int attempt = 0;
+
+            Console.WriteLine("Press enter to roll the dice");
+
+            while (roll != 6)
+            {
+                Console.ReadKey();
+                roll = numberGen.Next(1, 7);
+                Console.WriteLine("You rolled " + roll);
+                attempt++;
+            }
+
+            Console.WriteLine("It took " + attempt + " attempts to roll a six!");
+
+            // wait before closing
+            Console.ReadKey();
+        }
+    }
+}
